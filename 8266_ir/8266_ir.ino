691ad3c8a3868ed86ac52c1299691bf9b6a3a8f3
pengxin <2235880905@qq.com>
2021-08-03 18:14:22 +0800

    [M]重大更新：现在支持温度控制了

--- 8266_ir/8266_ir.ino
+++ 8266_ir/8266_ir.ino
@@ -1,108 +1,581 @@
-#define BLINKER_PRINT Serial
-#define BLINKER_MIOT_OUTLET
+/*-------------------------*/
+//原创代码 作者 bilibili @1812z 博客: 1812z.top 
+//由佛性进行适配艾韵智能
+//本代码仅支持美的空调别的请自行修改
 #define BLINKER_WIFI
+#define BLINKER_MIOT_AIRCONDITION
+#include <Arduino.h>
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
+#include <ir_Midea.h>  //修改空调型号改这里，比如你的空调是格力，那么使用下面的，并把这个添加注释
+#define ACSEND \
+   ac.send();  \
+   digitalWrite(kIrLed,HIGH);
+   
+char auth[] = "9cea7c06ec25";
+char ssid[] = "ASUS";
+char pswd[] = "887385206";
+
+//#include <ir_Gree.h> //左边是格力空调
+//
+//
+//
 #include <Blinker.h>
+const uint16_t kIrLed = 14;
+IRMideaAC ac(kIrLed);     // Set the GPIO used for sending messages.
+//DHT21，如果为
+// #define DHTTYPE DHT21  
+//#define DHTTYPE DHT22 
+#define DHTTYPE DHT11 
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#define DHTPIN 4
 
-char auth[] = "";//这里面填写设备的密钥
-char ssid[] = "ASUS";//这里填写你的wifi名字
-char pswd[] = "887385206";//这里填写你的WiFi密码
-//空调开
-uint16_t power_on[199] = {4334, 4348,  536, 1620,  534, 552,  562, 1592,  564, 1592,  534, 554,  534, 554,  566, 1590,  534, 554,  534, 554,  536, 1618,  534, 556,  534, 554,  566, 1586,  534, 1622,  532, 556,  534, 1620,  536, 1618,  564, 526,  536, 552,  534, 1622,  534, 1620,  534, 1620,  534, 1620,  562, 1590,  562, 526,  534, 1618,  566, 1590,  562, 526,  534, 554,  534, 556,  534, 556,  534, 554,  534, 1620,  564, 1590,  566, 524,  562, 1590,  534, 556,  562, 526,  534, 556,  532, 556,  534, 556,  534, 554,  536, 1618,  534, 554,  564, 1590,  536, 1618,  536, 1618,  536, 1618,  534, 5174,  4360, 4324,  532, 1620,  590, 498,  594, 1560,  536, 1620,  534, 554,  562, 526,  536, 1618,  536, 554,  536, 554,  562, 1592,  534, 554,  534, 556,  562, 1592,  564, 1590,  536, 554,  534, 1620,  534, 1620,  536, 554,  564, 524,  536, 1620,  534, 1618,  536, 1620,  532, 1620,  562, 1592,  536, 554,  534, 1620,  564, 1590,  536, 556,  534, 554,  534, 554,  534, 554,  564, 526,  534, 1618,  562, 1592,  564, 524,  536, 1618,  534, 554,  536, 554,  534, 554,  536, 554,  560, 528,  564, 526,  536, 1618,  536, 554,  562, 1592,  534, 1620,  536, 1618,  534, 1620,  534};  // UNKNOWN 1C03CDD8
-//空调关
-uint16_t power_off[199] = {4332, 4374,  534, 1620,  586, 502,  536, 1592,  562, 1620,  588, 500,  588, 502,  586, 1542,  610, 504,  534, 556,  534, 1620,  536, 554,  532, 558,  534, 1620,  588, 1566,  588, 502,  590, 1564,  534, 556,  518, 1636,  588, 1566,  566, 1588,  586, 1566,  588, 504,  532, 1620,  588, 1566,  588, 1566,  588, 502,  588, 502,  586, 502,  586, 504,  534, 1620,  586, 502,  534, 554,  584, 1570,  532, 1596,  614, 1566,  588, 502,  532, 554,  588, 502,  588, 500,  588, 502,  532, 556,  586, 504,  588, 502,  586, 1568,  586, 1566,  534, 1620,  588, 1540,  560, 1620,  586, 5122,  4386, 4296,  534, 1622,  586, 504,  534, 1618,  588, 1566,  534, 530,  612, 504,  534, 1620,  588, 502,  588, 500,  588, 1566,  588, 502,  588, 502,  588, 1566,  534, 1622,  584, 504,  534, 1594,  560, 554,  586, 1568,  588, 1568,  586, 1566,  534, 1620,  586, 502,  534, 1620,  588, 1568,  534, 1620,  588, 500,  616, 474,  532, 558,  588, 498,  588, 1566,  588, 504,  588, 500,  586, 1568,  586, 1568,  588, 1568,  586, 502,  586, 502,  532, 556,  588, 502,  586, 500,  588, 502,  584, 478,  612, 502,  586, 1566,  588, 1542,  558, 1620,  534, 1620,  588, 1566,  564};  // COOLIX B27BE0
+#define Slider_1 "Slidert"//APP温度控制滑条
+BlinkerSlider Slider1(Slider_1);
+#define Slider_2 "Sliderf"//APP风速控制滑条
+BlinkerSlider Slider2(Slider_2);
 
-//定义红外发射的管脚
-const uint16_t kIrLed = 14;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
-IRsend irsend(kIrLed);  // Set the GPIO to be used to sending the message.
-bool airPower = false;
-/*美的空调红外键值读取*/
+DHT dht(DHTPIN, DHTTYPE);
+uint32_t read_time = 0;
+float humi_read, temp_read;
+void dataStorage()
+{
+    Blinker.dataStorage("cha-temp", temp_read);
+    Blinker.dataStorage("cha-humi", humi_read);
+}
+BlinkerNumber HUMI("humi");
+BlinkerNumber TEMP("temp");
+//
 
-// 新建Blinker组件对象
-BlinkerButton Button1("btn-pwron");
-BlinkerButton Button2("btn-pwroff");
+
+bool oState = false;
+bool hsState = false;
+bool vsState = false;
+bool ecoState = false;//节能
+bool slState = false;//睡眠
+uint8_t setLevel;
+uint8_t setTemp;
+void miotLevel(uint8_t level)
+{
+    BLINKER_LOG("need set level: ", level);
+    // 0:AUTO MODE, 1-3 LEVEL
+   switch (level)
+    {
+     case 0:
+     ac.setFan(MIDEA_AC_FAN_AUTO);
+     ACSEND
+     break;
+     case 1:
+     ac.setFan(MIDEA_AC_FAN_LOW);
+     ACSEND
+     break;
+      case 2:
+     ac.setFan(MIDEA_AC_FAN_MED);
+     ACSEND
+     break;
+      case 3:
+     ac.setFan(MIDEA_AC_FAN_HI);
+     ACSEND
+     break;
+       case 4:
+    //  ac.setFan(kMideaAcFanTurbo);
+    //  ACSEND
+     break;
+     default:
+     break;
+    }  
+    BlinkerMIOT.level(level);
+    BlinkerMIOT.print();
+}
+
+void miotTemp(uint8_t temp)
+{
+    BLINKER_LOG("need set temp: ", temp);
+    setTemp = temp;    
+    ac.setTemp(setTemp);
+    ACSEND
+    Slider1.print(temp);
+    BlinkerMIOT.temp(temp);
+    BlinkerMIOT.print();
+}
+
+//APP控制
+BlinkerButton Button1("BUTTON_1");
+BlinkerButton Button2("BUTTON_2");
+//模式控制
+BlinkerButton Button3("BUTTON_3");
+BlinkerButton Button4("BUTTON_4");
+BlinkerButton Button5("BUTTON_5");
+BlinkerButton Button6("BUTTON_6");
+BlinkerButton Button7("BUTTON_7");
+////以下代码为节能模式开关 如果你的空调支持自行取消注释
+//BlinkerButton Button8("BUTTON_8");
+
+ ////以下代码为睡眠模式开关 如果你的空调支持自行取消注释
+//BlinkerButton Button9("BUTTON_9");
+
+//温度APP控制
+void slider1_callback(int32_t value)
+{
+    BLINKER_LOG("get slider value: ", value);  
+     ac.setTemp(value);
+     ACSEND
+     BlinkerMIOT.temp(value);
+     BlinkerMIOT.print();
+}
+//风速控制
+void slider2_callback(int32_t value)
+{
+    BLINKER_LOG("get slider value: ", value);  
+    switch (value)
+    {
+     case 0:
+     ac.setFan(MIDEA_AC_FAN_AUTO);
+     ACSEND
+     break;
+     case 1:
+     ac.setFan(MIDEA_AC_FAN_LOW);
+     ACSEND
+     break;
+      case 2:
+     ac.setFan(MIDEA_AC_FAN_MED);
+     ACSEND
+     break;
+      case 3:
+     ac.setFan(MIDEA_AC_FAN_HI);
+     ACSEND
+     break;
+     default:
+     break;
+    }  
+     BlinkerMIOT.level(value);
+     BlinkerMIOT.print();
+}
+//求教
+//模式APP控制
+//初始化
+
+void miotMode(const String & mode, const String & state)
+{
+    // eco ECO节能模式
+    // anion 负离子
+    // heater 辅热功能
+    // dryer 干燥功能
+    // sleep 睡眠模式
+    // soft 柔风功能
+    // uv UV杀菌
+    // unsb un-straight-blowing 防直吹
+    
+    BLINKER_LOG("need set mode: ", mode, ", state:", state);
+
+    BlinkerMIOT.mode(mode, state);
+}
+
+void miotHSwingState(const String & state)
+{
+    BLINKER_LOG("need set HSwing state: ", state);
+    // horizontal-swing
+    if (state == BLINKER_CMD_ON) {
+        BlinkerMIOT.hswing("on");
+        BlinkerMIOT.print();
+
+        hsState = true;
+    }
+    else if (state == BLINKER_CMD_OFF) {
+        BlinkerMIOT.hswing("off");
+        BlinkerMIOT.print();
+
+        hsState = false;
+    }
+}
+void heartbeat()//心跳
+{ 
+   HUMI.print(humi_read);
+    TEMP.print(temp_read);
+}
+//APP控制开关
+void button1_callback(const String & state)
+{
+  
+    BLINKER_LOG("get button state: ", state);
+    if (state == BLINKER_CMD_ON) {
+        BLINKER_LOG("Toggle on!");
+        Button1.icon("icon_1");
+        Button1.color("#FF0000");
+        Button1.text("电源开");
+        Button1.print("on");   
+         ac.on();
+        ACSEND
+    }
+    else if (state == BLINKER_CMD_OFF) {
+        BLINKER_LOG("Toggle off!");
+        Button1.icon("icon_1");
+        Button1.color("#FF0000");
+        Button1.text("电源关");
+        // Button1.text("Your button name", "describe");
+        Button1.print("off");
+         ac.off();
+        ACSEND
+    }
+     else {
+        BLINKER_LOG("Get user setting: ", state);
+
+        Button1.icon("icon_1");
+        Button1.color("#FFFFFF");
+        Button1.text("?");
+        // Button1.text("Your button name", "describe");
+        Button1.print();
+
+}
+}
+//APP控制开关-扫风
+void button2_callback(const String & state)
+{
+  
+    BLINKER_LOG("get button state: ", state);
+    if (state == BLINKER_CMD_ON) {
+        BLINKER_LOG("Toggle on!");
+        Button2.icon("icon_1");
+        Button2.color("#FF0000");
+        Button2.text("扫风开");
+        Button2.print("on");   
+        ac.setSwingVToggle(true);
+        ACSEND
+        BlinkerMIOT.vswing("on");
+        BlinkerMIOT.print();
+         vsState = true;
+    }
+    else if (state == BLINKER_CMD_OFF) {
+        BLINKER_LOG("Toggle off!");
+        Button2.icon("icon_1");
+        Button2.color("#FF0000");
+        Button2.text("扫风关");
+        // Button1.text("Your button name", "describe");
+        Button2.print("off");
+        ac.setSwingVToggle(false);
+        ACSEND
+        BlinkerMIOT.vswing("off");
+        BlinkerMIOT.print();
+         vsState = false;
+    }
+     else {
+        BLINKER_LOG("Get user setting: ", state);
+
+        Button1.icon("icon_1");
+        Button1.color("#FFFFFF");
+        Button1.text("?");
+        // Button1.text("Your button name", "describe");
+        Button1.print();
+
+}
+}
+//APP控制-模式
+void button3_callback(const String & state)
+{
+  
+    BLINKER_LOG("get button state: ", state);
+    if (state == BLINKER_CMD_BUTTON_TAP) {
+        BLINKER_LOG("Toggle on!");
+        ac.setMode(MIDEA_AC_AUTO);
+        ACSEND
+    }
+}
+void button4_callback(const String & state)
+{
+  
+    BLINKER_LOG("get button state: ", state);
+    if (state == BLINKER_CMD_BUTTON_TAP) {
+        BLINKER_LOG("Toggle on!");  
+        ac.setMode(MIDEA_AC_COOL);
+        ACSEND
+    }
+}
+ void button5_callback(const String & state)
+{
+  
+    BLINKER_LOG("get button state: ", state);
+    if (state == BLINKER_CMD_BUTTON_TAP) {
+        BLINKER_LOG("Toggle on!"); 
+        ac.setMode(MIDEA_AC_DRY);
+        ACSEND
+    }
+}
+void button6_callback(const String & state)
+{
+  
+    BLINKER_LOG("get button state: ", state);
+    if (state == BLINKER_CMD_BUTTON_TAP) {
+        BLINKER_LOG("Toggle on!");
+        ac.setMode(MIDEA_AC_FAN);
+        ACSEND
+    }
+}
+void button7_callback(const String & state)
+{
+  
+    BLINKER_LOG("get button state: ", state);
+    if (state == BLINKER_CMD_BUTTON_TAP) {
+        BLINKER_LOG("Toggle on!"); 
+        ac.setMode(MIDEA_AC_HEAT);
+        ACSEND
+    }
+}  
+//以下代码为节能模式开关 如果你的空调支持自行取消注释
+//void button8_callback(const String & state)
+// 
+//   BLINKER_LOG("get button state: ", state);
+//   if (state == BLINKER_CMD_ON) {
+//       BLINKER_LOG("Toggle on!");
+//       Button8.icon("icon_1");
+//       Button8.text("节能开");
+//       Button8.print("on");   
+//       ac.eco(on);
+//       ACSEND
+//       BlinkerMIOT.eco("on");
+//       BlinkerMIOT.print();
+//        ecoState = true;
+//   }
+//   else if (state == BLINKER_CMD_OFF) {
+//       BLINKER_LOG("Toggle off!");
+//       Button8.icon("icon_1");
+//       Button8.color("#FF0000");
+//       Button8.text("节能关");
+//       // Button1.text("Your button name", "describe");
+//       Button8.print("off");
+//       ac.eco(false);
+//       ACSEND
+//        BlinkerMIOT.eco("off");
+//        BlinkerMIOT.print();
+//         ecoState = false;
+//   }
+//}
+//void button9_callback(const String & state)
+//{
+//void miotecoState(const String & state)
+//{
+//    BLINKER_LOG("need set eco state: ", state);
+//
+//    if (state == BLINKER_CMD_ON) {
+//        ac.eco(on);
+//        ACSEND
+//        BlinkerMIOT.eco("on");
+//        BlinkerMIOT.print();
+//        ecoState = true;
+//    }
+//    else if (state == BLINKER_CMD_OFF) {
+//        ac.eco(off);
+//        ACSEND
+//        BlinkerMIOT.eco("off");
+//        BlinkerMIOT.print();
+//        ecoState = false;
+//    }
+//}
+
+
+//以下代码为睡眠模式开关 如果你的空调支持自行取消注释
+//  BLINKER_LOG("get button state: ", state);
+//    if (state == BLINKER_CMD_ON) {
+//        BLINKER_LOG("Toggle on!");
+//        Button9.icon("icon_1");
+//        Button9.text("睡眠开");
+//        Button9.print("on");   
+//        ac.sleep(on);
+//        ACSEND
+//        BlinkerMIOT.sleep("on");
+//        BlinkerMIOT.print();
+//        slState = true;
+//    }
+//    else if (state == BLINKER_CMD_OFF) {
+//        BLINKER_LOG("Toggle off!");
+//        Button9.icon("icon_1");
+//        Button9.color("#FF0000");
+//        Button9.text("睡眠关");
+//        // Button1.text("Your button name", "describe");
+//        Button9.print("off");
+ //       ac.sleep(false);
+//        ACSEND
+//        BlinkerMIOT.sleep("off");
+//        BlinkerMIOT.print();
+//         slState = false;
+//    }
+//}
+//void miotsleepState(const String & state)
+//{
+//    BLINKER_LOG("need set sleep state: ", state);
+//
+//    if (state == BLINKER_CMD_ON) {
+//        ac.sleep(on);
+//        ACSEND
+//        BlinkerMIOT.sleep("on");
+//        BlinkerMIOT.print();
+//        slState = true;
+//    }
+//    else if (state == BLINKER_CMD_OFF) {
+//        ac.sleep(off);
+//        ACSEND
+//        BlinkerMIOT.sleep("off");
+//        BlinkerMIOT.print();
+//        slState = false;
+//    }
+//}
+
+
+void miotVSwingState(const String & state)
+{
+    BLINKER_LOG("need set VSwing state: ", state);
+    // vertical-swing
+
+    if (state == BLINKER_CMD_ON) {
+        BlinkerMIOT.vswing("on");
+        BlinkerMIOT.print();
+        ac.setSwingVToggle(true);
+        ACSEND
+        vsState = true;
+    }
+    else if (state == BLINKER_CMD_OFF) {
+        BlinkerMIOT.vswing("off");
+        BlinkerMIOT.print();
+        ac.setSwingVToggle(false);
+        ACSEND
+        vsState = false;
+    }
+}
+//小爱电源控制
+void miotPowerState(const String & state)
+{
+    BLINKER_LOG("need set power state: ", state);
+
+    if (state == BLINKER_CMD_ON) {
+        ac.on();
+        ACSEND
+        BlinkerMIOT.powerState("on");
+        BlinkerMIOT.print();
+        oState = true;
+    }
+    else if (state == BLINKER_CMD_OFF) {
+        ac.off();
+        ACSEND
+        BlinkerMIOT.powerState("off");
+        BlinkerMIOT.print();
+        oState = false;
+    }
+}
+
+void miotQuery(int32_t queryCode)
+{
+    BLINKER_LOG("MIOT Query codes: ", queryCode);
+
+    switch (queryCode)
+    {
+        case BLINKER_CMD_QUERY_ALL_NUMBER :
+            BLINKER_LOG("MIOT Query All");
+            BlinkerMIOT.powerState(oState ? "on" : "off");
+            BlinkerMIOT.hswing(hsState ? "on" : "off");
+            BlinkerMIOT.vswing(vsState ? "on" : "off");
+            BlinkerMIOT.eco("off");
+            BlinkerMIOT.anion("off");
+            BlinkerMIOT.heater("off");
+            BlinkerMIOT.dryer("off");
+            BlinkerMIOT.sleep(slState ? "on" : "off");
+            BlinkerMIOT.soft("off");
+            BlinkerMIOT.uv("off");
+            BlinkerMIOT.unStraightBlow("off");
+            BlinkerMIOT.level(setLevel);
+            BlinkerMIOT.temp(setTemp);       
+            BlinkerMIOT.print();
+            break;
+        case BLINKER_CMD_QUERY_POWERSTATE_NUMBER :
+            BLINKER_LOG("MIOT Query Power State");
+            BlinkerMIOT.powerState(oState ? "on" : "off");
+            BlinkerMIOT.print();
+            break;
+        default :
+            BlinkerMIOT.powerState(oState ? "on" : "off");
+            BlinkerMIOT.hswing(hsState ? "on" : "off");
+            BlinkerMIOT.vswing(vsState ? "on" : "off");
+            BlinkerMIOT.eco("off");
+            BlinkerMIOT.anion("off");
+            BlinkerMIOT.heater("off");
+            BlinkerMIOT.dryer("off");
+            BlinkerMIOT.sleep("off");
+            BlinkerMIOT.soft("off");
+            BlinkerMIOT.uv("off");
+            BlinkerMIOT.unStraightBlow("off");
+            BlinkerMIOT.level(setLevel);
+            BlinkerMIOT.temp(setTemp);  
+            BlinkerMIOT.print();
+            break;
+    }
+}
+
+void dataRead(const String & data)
+{
+    BLINKER_LOG("Blinker readString: ", data);
+
+    Blinker.vibrate();
+    
+    uint32_t BlinkerTime = millis();
+    
+    Blinker.print("millis", BlinkerTime);
+}
 
 void setup()
 {
-  //初始化串口
-  Serial.begin(115200);
-  BLINKER_DEBUG.stream(Serial);
- //配网步骤：WIFI接入
-  Blinker.begin(auth,ssid,pswd); 
-  Button1.attach(Button1_callback);
-  Button2.attach(Button2_callback);
-  BlinkerMIOT.attachPowerState(miotPowerState);   //语音指令与控制函数对应（开关指令）
-  BlinkerMIOT.attachQuery(miotQuery);    //语音指令与控制函数对应(查询指令)
-  //红外初始化
-  irsend.begin();
-  pinMode(14,OUTPUT);
-  digitalWrite(14,HIGH);
-
+  // 
+    ac.begin();
+    Serial.begin(115200);
+    BLINKER_DEBUG.stream(Serial);
+//APP控制
+    Button1.attach(button1_callback);
+    Button2.attach(button2_callback);
+    //模式callback
+    Button3.attach(button3_callback);
+    Button4.attach(button4_callback);
+    Button5.attach(button5_callback);
+    Button6.attach(button6_callback);
+    Button7.attach(button7_callback);
+    ////以下代码为节能模式开关 如果你的空调支持自行取消注释
+   // Button8.attach(button8_callback);//节能
+   // ////以下代码为睡眠模式开关 如果你的空调支持自行取消注释
+   // Button9.attach(button9_callback);//睡眠
+    //
+    Blinker.begin(auth, ssid, pswd);
+    Blinker.attachData(dataRead);
+    //DHT
+    Blinker.attachHeartbeat(heartbeat);
+     Blinker.attachDataStorage(dataStorage);
+     dht.begin();
+     //
+    BlinkerMIOT.attachPowerState(miotPowerState);
+    BlinkerMIOT.attachHSwing(miotHSwingState);
+    BlinkerMIOT.attachVSwing(miotVSwingState);
+    BlinkerMIOT.attachLevel(miotLevel);
+    BlinkerMIOT.attachMode(miotMode);
+    BlinkerMIOT.attachTemp(miotTemp);
+    BlinkerMIOT.attachQuery(miotQuery);
+    Slider1.attach(slider1_callback);//温度
+    Slider2.attach(slider2_callback);//风速
+    pinMode(kIrLed,OUTPUT);
+    digitalWrite(kIrLed,HIGH);
 }
+
 void loop()
 {
-  Blinker.run(); 
-}
-
-//回调函数
-void Button1_callback(const String &state)
-{
-
-  BLINKER_LOG("get button state: ", state);
-  //检测到普通按钮按下（btn-pwron）
-  if (state == BLINKER_CMD_BUTTON_TAP)
-  {
-    //发送红外指令
-    Serial.println("打开空调");
-    irsend.sendRaw(power_on, 199, 38);  // Send a raw data capture at 38kHz.
-    digitalWrite(14,HIGH);
-    Button1.print("2楼空调开");
-    airPower = true;
-  }
-
-}
-//回调函数
-void Button2_callback(const String &state)
-{
-  BLINKER_LOG("get button state: ", state);
-  //检测到btn-pwroff按钮按下
-  if (state == BLINKER_CMD_BUTTON_TAP)
-  {
-    Serial.println("关闭空调");
-    irsend.sendRaw(power_off, 199, 38);  // Send a raw data capture at 38kHz.
-    digitalWrite(14,HIGH);
-    Button2.print("2楼空调关");
-    airPower = false;
-  }
-}
-
-void miotPowerState(const String & state)    //小爱语音开关控制进入函数
-{ 
-    if (state == BLINKER_CMD_ON){     //判断语音内容 为打开
-      Button1.print("on");          //点灯科技软件对应按键被打开
-      BLINKER_LOG("空调打开");   
-      BlinkerMIOT.powerState("on");   //反馈小爱同学按键状态已打开
-      BlinkerMIOT.print();            //发送反馈
-      irsend.sendRaw(power_on, 199, 38);     //发射电视开关键的红外数据（同上）
-      digitalWrite(14,HIGH);
-      delay(20); 
-      airPower = true;                  //开关状态保存，用于小爱同学与BLINKER同步
+    Blinker.run();
+    //dht
+      float h = dht.readHumidity();
+      float t = dht.readTemperature();
+    if (isnan(h) || isnan(t))
+    {
+        BLINKER_LOG("Failed to read from DHT sensor!");
     }
-    else if (state == BLINKER_CMD_OFF){  //判断语音内容 为关闭
-      Button2.print("off");            //点灯科技软件对应按键被关闭
-      BLINKER_LOG("电视关闭");
-      BlinkerMIOT.powerState("off"); //反馈小爱同学按键状态已关闭
-      BlinkerMIOT.print();           //发送反馈
-      irsend.sendRaw(power_off, 199, 38);    //发射电视开关键的红外数据（同上）
-      digitalWrite(14,HIGH);
-      delay(20); 
-      airPower = false;                //开关状态保存，用于小爱同学与BLINKER同步
-    }
+    else
+    {
+       // BLINKER_LOG("Humidity: ", h, " %");
+       //BLINKER_LOG("Temperature: ", t, " *C");
+        humi_read = h;
+        temp_read = t;
+        float hic = dht.computeHeatIndex(t, h, false);
 }
-
-void miotQuery(int32_t queryCode)//小爱语音查询状态函数
-{
-    BlinkerMIOT.powerState(airPower ? "on" : "off");  //可查询开关状态
-    BlinkerMIOT.print();
 }
